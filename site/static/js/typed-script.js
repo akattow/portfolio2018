4be9baa938c9c979606544a5fd0902b34ab6ca02
Kat Tow <kattow88@gmail.com>
2018-05-16 12:06:08 -0500

    cont. attempt to resolve typed.js

--- site/static/js/typed-script.js
+++
@@ -1,9 +0,0 @@
-$(function () {
-  $(".element").typed({
-    strings: ["digital storytelling.", "user-centered design.", "creative problem-solving.", "taking photos of my dogs."],
-    typeSpeed: 0,
-    backDelay: 800,
-    loop: true,
-    loopCount: null,
-  });
-});
