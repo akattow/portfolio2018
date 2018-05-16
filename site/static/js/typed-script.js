6865c9b6adef6a7bee1aeb7c9b46405098e77e61
Kat Tow <kattow88@gmail.com>
2018-05-16 12:08:59 -0500

    replaced js folder in static resources

---
+++ site/static/js/typed-script.js
@@ -0,0 +1,9 @@
+$(function () {
+  $(".element").typed({
+    strings: ["digital storytelling.", "user-centered design.", "creative problem-solving.", "taking photos of my dogs."],
+    typeSpeed: 0,
+    backDelay: 800,
+    loop: true,
+    loopCount: null,
+  });
+});
